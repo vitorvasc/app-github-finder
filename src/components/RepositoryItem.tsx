b0b0c65f9c96aba0552e08a9698b0027aab77c83
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2025-02-14 15:07:46 -0300

    feat: create component for RepositoryItem

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,55 @@
+import StarIcon from '@/assets/icons/star.svg';
+import WatcherIcon from '@/assets/icons/watcher.svg';
+import RepositoryIcon from '@/assets/icons/repository.svg';
+import Image from 'next/image';
+
+interface RepositoryItemProps {
+  id: number;
+  name: string;
+  description?: string | null;
+  url: string;
+  stars_count: number;
+  watchers_count: number;
+}
+
+export function RepositoryItem({
+  id,
+  name,
+  description,
+  url,
+  stars_count,
+  watchers_count,
+}: RepositoryItemProps) {
+  return (
+    <li key={id} className='py-3 sm:py-4'>
+      <div className='flex items-center space-x-4 rtl:space-x-reverse'>
+        <div className='shrink-0'>
+          <Image src={RepositoryIcon} alt='Repository' />
+        </div>
+        <div className='flex-1 min-w-0'>
+          <a
+            href={url}
+            target='_blank'
+            rel='noopener noreferrer'
+            className='text-l-1 text-blue-700 truncate hover:underline'
+          >
+            {name}
+          </a>
+          <p className='text-sm font-light mt-1 text-neutral-600 truncate'>
+            {description}
+          </p>
+        </div>
+        <div className='flex flex-col space-y-1 text-sm font-light text-neutral-700'>
+          <div className='flex align-center space-x-1'>
+            <Image src={StarIcon} alt='Stars' width='18' height='18' />
+            <span>{stars_count} stars</span>
+          </div>
+          <div className='flex align-center space-x-1'>
+            <Image src={WatcherIcon} alt='Watchers' width='18' height='18' />
+            <span>{watchers_count} watchers</span>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+}
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import StarIcon from '@/assets/icons/star.svg';
-import WatcherIcon from '@/assets/icons/watcher.svg';
-import Image from 'next/image';
 import { Repository, SortField, SortOrder } from '@/types';
 import { useState } from 'react';
+import { RepositoryItem } from './RepositoryItem';
 
 interface RepositoryListProps {
   repositories: Repository[];
@@ -49,45 +47,15 @@ export function RepositoryList({ repositories }: RepositoryListProps) {
 
       <ul className='max-w md bg-white rounded-lg shadow-lg p-6 divide-y divide-gray-200 dark:divide-gray-700'>
         {sortedRepositories.map((repo) => (
-          <li key={repo.id} className='py-3 sm:py-4'>
-            <div className='flex items-center space-x-4 rtl:space-x-reverse'>
-              <div className='shrink-0'>
-                <img
-                  src='/docs/images/people/profile-picture-1.jpg'
-                  alt=''
-                  className='w-8 h-8 rounded-full'
-                />
-              </div>
-              <div className='flex-1 min-w-0'>
-                <a
-                  href={repo.html_url}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='text-xl font-medium text-neutral-700 truncate hover:underline'
-                >
-                  {repo.name}
-                </a>
-                <p className='text-md mt-2 text-neutral-600 truncate'>
-                  {repo.description}
-                </p>
-              </div>
-              <div className='flex flex-col space-y-1 text-sm font-light text-neutral-700'>
-                <div className='flex align-center space-x-1'>
-                  <Image src={StarIcon} alt='Stars' width='18' height='18' />
-                  <span>{repo.stargazers_count} stars</span>
-                </div>
-                <div className='flex align-center space-x-1'>
-                  <Image
-                    src={WatcherIcon}
-                    alt='Watchers'
-                    width='18'
-                    height='18'
-                  />
-                  <span>{repo.watchers_count} watchers</span>
-                </div>
-              </div>
-            </div>
-          </li>
+          <RepositoryItem
+            key={repo.id}
+            id={repo.id}
+            url={repo.html_url}
+            name={repo.name}
+            description={repo.description}
+            stars_count={repo.stargazers_count}
+            watchers_count={repo.watchers_count}
+          />
         ))}
       </ul>
     </div>
