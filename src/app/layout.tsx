ea4bcacfca1148cdca0abc9274f7f1ff5fb46b10
Vitor Vasconcellos <vvasconcellos1@gmail.com>
2025-02-16 18:38:20 -0300

    feat: add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>
-        <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+      <body className={`${inter.className} flex flex-col min-h-screen`}>
+        <div className='flex-grow'>
+          <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+        </div>
+
+        <footer className='w-full py-4 text-center bg-white border-t'>
+          <p className='text-sm text-gray-600'>
+            Created by {' '}
+            <a
+              href='https://github.com/vitorvasc'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-blue-500 hover:text-blue-600 transition-colors'
+            >
+              vitorvasc
+            </a>
+          </p>
+        </footer>
       </body>
     </html>
   );
